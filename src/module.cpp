87e4cd76ce53deeae3a718ba4252524c003b89fd
BazzahN <72982319+BazzahN@users.noreply.github.com>
2024-03-27 15:05:34 +0000

    Add files via upload

--- src/module.cpp
+++ src/module.cpp
@@ -7,7 +7,7 @@ using namespace Rcpp;
 #include <vector>
 #include <map>
 
-std::list<std::vector<int>> stable_matcher(Rcpp::NumericVector cv, Rcpp::NumericVector A, Rcpp::NumericVector B)
+std::list<std::vector<int>> stable_matcher_stble(const Rcpp::NumericVector& cv, const Rcpp::NumericVector& A, const Rcpp::NumericVector& B)
 {
     std::map<int, std::vector<int>> A_prefs;
     std::map<int, std::vector<int>> B_prefs;	
@@ -57,8 +57,56 @@ std::list<std::vector<int>> stable_matcher(Rcpp::NumericVector cv, Rcpp::Numeric
   
 }
 
+std::list<std::vector<int>> stable_matcher_intgr(const int& n, const Rcpp::NumericVector& A, const Rcpp::NumericVector& B)
+{
+    std::map<int, std::vector<int>> A_prefs;
+    std::map<int, std::vector<int>> B_prefs;
+  
+    std::vector<int> hld_1;
+    std::vector<int> hld_2;
+    int j {0}; 	
+    R_xlen_t k = n;	
+    for (R_xlen_t i = 1; i < A.size() +1; i++) 
+    {
+  
+    	hld_1.push_back(A[i-1]);
+	hld_2.push_back(B[i-1]);
+    
+   	if(i % k  == 0)
+    	{
+    	  A_prefs.insert(std::make_pair(j, hld_1));
+	  B_prefs.insert(std::make_pair(j, hld_2));
+      	  j++;
+          hld_1.clear();
+	  hld_2.clear();
+    
+    	}
+    
+   }
+
+   std::list<std::vector<int>> sol = fun_algo(A_prefs,B_prefs,false);
+
+   Rcout <<"Stable Match Found! \n" << std::endl;
+
+   Rcout << "Identified Pairings:" << std::endl;
+   Rcout << "Female |" << " " << "Male" << std::endl;
+
+        for(auto i : sol)
+        {
+
+            Rcout << "  " << i[0] << "    |   "  << i[1] << std::endl;
+            Rcout << "-------|-------" << std::endl;
+        }
+        
+
+	
+  return sol;
+  
+}
+
 
 RCPP_MODULE(stablepack) 
 {
-function("rcpp_stable_matcher", &stable_matcher);
+function("rcpp_stable_matcher", &stable_matcher_stble);
+function("rcpp_stable_int", &stable_matcher_intgr);
 }
