69b0438e74f7c3bca46e0a4ce0635cac42f1956f
BazzahN <72982319+BazzahN@users.noreply.github.com>
2024-03-28 15:58:24 +0000

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,2 @@
 std::vector<int> pref_subtract(std::vector<int> a, std::vector<int> b);
 bool in(const int& key, const std::vector<int>& v);
-std::map<int,std::vector<int>> file_rw_pref_dict(const std::string& filename);
